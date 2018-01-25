68d546abb00362efb8a60ec53511df429273cfed
BuildTools <nur1popcorn@gmail.com>
2018-01-25 23:44:50 +0100

    Nearly done with MaliciousCodeScanner.

--- src/main/java/com/nur1popcorn/basm/transformers/MaliciousCodeScanner.java
+++ src/main/java/com/nur1popcorn/basm/transformers/MaliciousCodeScanner.java
@@ -4,19 +4,116 @@ import com.nur1popcorn.basm.classfile.ConstantPool;
 import com.nur1popcorn.basm.classfile.constants.ConstantInfo;
 import com.nur1popcorn.basm.classfile.constants.ConstantMethodRef;
 import com.nur1popcorn.basm.classfile.constants.ConstantNameAndType;
+import com.sun.javadoc.*;
+import com.sun.tools.javac.util.Context;
+import com.sun.tools.javac.util.ListBuffer;
+import com.sun.tools.javadoc.*;
 
+import javax.tools.JavaFileObject;
 import java.io.*;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static com.nur1popcorn.basm.utils.Constants.*;
 
-public class MaliciousCodeScanner implements Transformer {
+public final class MaliciousCodeScanner implements Transformer {
 
     private static final String MALICIOUS_PATHS[] = {
         "java/io",
-        "java/net",
-        "java/lang/reflect"
+        "java/lang/reflect",
+        "java/net"
     };
+    
+    private final Map<String, RootDoc> DOC_MAP = new HashMap<>();
+
+    private static final com.sun.tools.javac.util.List<?> EMPTY_LIST = new ListBuffer<>().toList();
+    private static JavadocTool tool;
+
+    static {
+        final Context context = new Context();
+        try {
+            final Constructor constructor = Messager.class.getDeclaredConstructor(Context.class, String.class);
+            constructor.setAccessible(true);
+            constructor.newInstance(context, "MaliciousCodeScanner"); // instance is used by JavadocTool.
+        } catch (InstantiationException | IllegalAccessException | NoSuchMethodException | InvocationTargetException e) {
+            e.printStackTrace();
+            throw new RuntimeException();
+        }
+        tool = JavadocTool.make0(context);
+        if(tool == null) {
+            System.out.println("shit.");
+            throw new RuntimeException();
+        }
+    }
+
+    private RootDoc getRootDoc(String clazz) throws IOException {
+        RootDoc doc = DOC_MAP.get(clazz);
+        if(doc != null)
+            return doc;
+        doc = tool.getRootDocImpl(
+                "",
+                null,
+                new ModifierFilter(0),
+                ListBuffer.of(clazz).toList(),
+                (com.sun.tools.javac.util.List<String[]>) EMPTY_LIST,
+                (com.sun.tools.javac.util.List<JavaFileObject>) EMPTY_LIST,
+                false,
+                (com.sun.tools.javac.util.List<String>) EMPTY_LIST,
+                (com.sun.tools.javac.util.List<String>) EMPTY_LIST,
+                false,
+                false,
+                true
+        );
+        DOC_MAP.put(clazz, doc);
+        return doc;
+    }
+
+    private static String getMethodDesc(ExecutableMemberDoc method) {
+        final StringBuilder stringBuilder = new StringBuilder()
+                .append("(");
+        for(Parameter parameter : method.parameters()) {
+            final String type = parameter.type()
+                    .qualifiedTypeName();
+            // TODO: arrays.
+            // based on table 4.3-A: https://docs.oracle.com/javase/specs/jvms/se8/html/jvms-4.html#jvms-4.3.2
+            switch(type) {
+                case "byte":
+                    stringBuilder.append("B");
+                    break;
+                case "char":
+                    stringBuilder.append("C");
+                    break;
+                case "double":
+                    stringBuilder.append("D");
+                    break;
+                case "float":
+                    stringBuilder.append("F");
+                    break;
+                case "int":
+                    stringBuilder.append("I");
+                    break;
+                case "long":
+                    stringBuilder.append("J");
+                    break;
+                default:
+                    stringBuilder.append("L")
+                            .append(type.replace(".", "/"))
+                            .append(";");
+                    break;
+                case "short":
+                    stringBuilder.append("S");
+                    break;
+                case "boolean":
+                    stringBuilder.append("Z");
+                    break;
+            }
+        }
+        return stringBuilder.append(")")
+                .toString();
+    }
 
     @Override
     public void transform(List<InputStream> in, OutputStream out) throws IOException {
@@ -44,16 +141,32 @@ public class MaliciousCodeScanner implements Transformer {
                                         writer.print(path);
                                         writer.print("#");
                                         final ConstantNameAndType nameAndType = methodRef.indexNameAndType(constantPool);
-                                        writer.print(nameAndType.indexName(constantPool).bytes);
-                                        writer.println(nameAndType.indexDesc(constantPool).bytes);
-                                        //TODO: fetch java docs and display ?
-                                        break;
+                                        final String name = nameAndType.indexName(constantPool).bytes;
+                                        String desc = nameAndType.indexDesc(constantPool).bytes;
+                                        writer.print(name);
+                                        writer.println(desc);
+
+                                        final ClassDoc docs[] = getRootDoc("src/" + path + ".java").classes();
+                                        if(docs.length != 0) {
+                                            desc = desc.substring(0, desc.indexOf(')') + 1);
+                                            if(name.equals("<init>")) {
+                                                for(ConstructorDoc constructor : docs[0].constructors())
+                                                    if(getMethodDesc(constructor).equals(desc)) {
+                                                        writer.println(constructor.getRawCommentText());
+                                                        break;
+                                                    }
+                                            } else
+                                                for(MethodDoc method : docs[0].methods())
+                                                    if(method.name().equals(name) &&
+                                                       getMethodDesc(method).equals(desc)) {
+                                                        writer.println(method.getRawCommentText());
+                                                        break;
+                                                    }
+                                        }
                                     }
-                            } else if(constantInfo.getTag() == CONSTANT_CLASS)
-                                ;
+                            }
                     }
                 }
-
                 writer.flush();
             } catch (IOException e) {
                 e.printStackTrace();
