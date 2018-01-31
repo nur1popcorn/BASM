5167914417ef5de04cc1e22f8fee871a8d05928d
BuildTools <nur1popcorn@gmail.com>
2018-01-31 17:55:34 +0100

    Added some more stuff to ClassReader.

--- src/main/java/com/nur1popcorn/basm/classfile/ClassReader.java
+++ src/main/java/com/nur1popcorn/basm/classfile/ClassReader.java
@@ -1,16 +1,69 @@
+/*
+ * Copyright (C) Keanu Poeschko - All Rights Reserved
+ * Unauthorized copying of this file is strictly prohibited
+ *
+ * Created by Keanu Poeschko <nur1popcorn@gmail.com>, August 2017
+ * This file is part of {BASM}.
+ *
+ * Do not copy or distribute files of {BASM} without permission of {Keanu Poeschko}
+ *
+ * Permission to use, copy, modify, and distribute my software for
+ * educational, and research purposes, without a signed licensing agreement
+ * and for free, is hereby granted, provided that the above copyright notice
+ * and this paragraph appear in all copies, modifications, and distributions.
+ *
+ * {BASM} is based on this document: https://docs.oracle.com/javase/specs/jvms/se8/html/jvms-4.html
+ *
+ */
+
 package com.nur1popcorn.basm.classfile;
+
+import com.nur1popcorn.basm.classfile.constants.ConstantName;
+import com.nur1popcorn.basm.classfile.constants.ConstantUtf8;
 
 import java.io.DataInputStream;
 import java.io.IOException;
 
 import static com.nur1popcorn.basm.utils.Constants.*;
 
-public class ClassReader {
+/**
+ * The {@link ClassReader} is used for reading the full or parts of the class provided.
+ * <a href="https://docs.oracle.com/javase/specs/jvms/se8/html/jvms-4.html#jvms-4.1">
+ *     ClassFile 4.1.
+ * </a>
+ *
+ * @see ConstantPool
+ * @see FieldMethodInfo
+ * @see IClassReaderVisitor
+ *
+ * @author nur1popcorn
+ * @since 1.0.0-alpha
+ */
+public final class ClassReader {
+
+    /**
+     * <b>Read head</b>
+     *
+     * <p>This flag enables reading the head part of the JavaClass.</p>
+     * <p>These are considered part of the head:</p>
+     * <ul>
+     *     <li>minor version</li>
+     *     <li>major version</li>
+     *     <li>constantpool</li>
+     * </ul>
+     */
     public static final int READ_HEAD = 0x1;
+
+    /**
+     * <b>Read body</b>
+     */
     public static final int READ_BODY = 0x2;
     public static final int READ_FIELDS = 0x4;
     public static final int READ_METHODS = 0x8;
 
+    /**
+     * A table which can be indexed to obtain the expected size of any {@link ConstantPool} entry.
+     */
     private static final int CONSTANT_INFO_SKIP_TABLE[] = {
         0,
         0, // CONSTANT_Utf8
@@ -43,7 +96,7 @@ public class ClassReader {
     private int access;
 
     private int thisClass,
-            superClass;
+                superClass;
 
     private int interfaces[];
     private FieldMethodInfo fields[];
@@ -85,25 +138,71 @@ public class ClassReader {
     }
 
     public void accept(IClassReaderVisitor visitor, int read) throws IOException {
+        assert(read & READ_HEAD) != 0 &&
+               ((read & READ_BODY) == 0 ||
+                (read & READ_FIELDS) == 0 ||
+                (read & READ_METHODS) == 0);
         if((read & READ_HEAD) != 0) {
             readHead();
             visitor.visit(constantPool);
         } else {
             // skip minor/major version
             in.skipBytes(4);
+            // https://docs.oracle.com/javase/specs/jvms/se8/html/jvms-4.html#jvms-4.4
             final int cpSize = in.readUnsignedShort();
-            for(int i = 1 /* the cp's size is 1 less than given */; i < cpSize; i++)
-                switch(in.readByte()) {
+            for(int i = 1 /* the cp's size is 1 less than given */; i < cpSize; i++) {
+                final byte tag = in.readByte();
+                switch(tag) {
                     case CONSTANT_UTF8:
                         in.skipBytes(in.readUnsignedShort());
                         break;
                     case CONSTANT_LONG:
                     case CONSTANT_DOUBLE:
                         i++ /* padding */;
-                        break;
                     default:
-                        in.skipBytes(CONSTANT_INFO_SKIP_TABLE[i]);
+                        in.skipBytes(CONSTANT_INFO_SKIP_TABLE[tag]);
                 }
+            }
         }
+
+        if((read & (READ_BODY | READ_FIELDS | READ_METHODS)) == 0) {
+            in.close();
+            return;
+        }
+
+        if((read & READ_BODY) != 0) {
+            readBody();
+            final ConstantUtf8 interfaces[] = new ConstantUtf8[this.interfaces.length];
+            for(int i = 0; i < interfaces.length; i++)
+                interfaces[i] = ((ConstantName)constantPool.getEntry(this.interfaces[i]))
+                                        .indexName(constantPool);
+            visitor.visit(access,
+                          ((ConstantName)constantPool.getEntry(thisClass))
+                                  .indexName(constantPool),
+                          ((ConstantName)constantPool.getEntry(superClass))
+                                  .indexName(constantPool),
+                          interfaces);
+        } else {
+            // skip accessFlags, thisClass and superClass.
+            in.skipBytes(6);
+            // skip interfaces.
+            in.skipBytes(in.readUnsignedShort());
+        }
+
+        if((read & (READ_FIELDS | READ_METHODS)) == 0) {
+            in.close();
+            return;
+        }
+
+        if((read & (READ_FIELDS)) != 0) {
+
+        } else {
+
+        }
+
+        if((read & READ_METHODS) != 0) {
+
+        }
+        in.close();
     }
 }
